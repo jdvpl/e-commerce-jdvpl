bd390395bfb34d53cacd1c676bc9525259684425
Juan Daniel <juanda554242@gmail.com>
2021-03-07 23:03:03 -0500

    navegacion de Mitienda

---
+++ ecommerce expo poryecto/ecommerce-jdvpl/src/navigation/MitiendaStack.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Mitienda from "../screens/MiTienda/Mitienda";
+import EditarProducto from "../screens/MiTienda/EditarProducto";
+
+const Stack = createStackNavigator();
+
+const MitiendaStack = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#007aff", headerTintColor: "#fff" },
+      }}
+    >
+      <Stack.Screen
+        component={Mitienda}
+        name="mitienda"
+        options={{
+          title: "Mi Tienda",
+        }}
+      />
+
+      <Stack.Screen
+        component={EditarProducto}
+        name="editar"
+        options={{
+          title: "Editar Porducto",
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default MitiendaStack;
+
+const styles = StyleSheet.create({});
