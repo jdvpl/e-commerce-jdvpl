d00b15d2cfef690a4482f1a405e5ea7c8ea24347
Juan Daniel <juanda554242@gmail.com>
2021-03-07 22:52:34 -0500

    configurando la nevegacion

--- ecommerce expo poryecto/ecommerce-jdvpl/src/navigation/TiendaStack.js
+++ ecommerce expo poryecto/ecommerce-jdvpl/src/navigation/TiendaStack.js
@@ -10,7 +10,55 @@ import Mensajes from "../screens/tienda/Mensajes";
 const Stack = createStackNavigator();
 
 export default function TiendaStack() {
-  return <Stack.Navigator></Stack.Navigator>;
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        component={Tienda}
+        name="tienda"
+        options={{ headerShown: false }}
+      />
+
+      <Stack.Screen
+        component={AgregarProducto}
+        name="agregarproducto"
+        options={{
+          headerShown: false,
+          headerStyle: { backgroundColor: "#007aff" },
+          headerTintColor: "#fff",
+        }}
+      />
+
+      <Stack.Screen
+        component={Detalle}
+        name="detalle"
+        options={{
+          headerTransparent: true,
+          headerTintColor: "#007aff",
+          title: "",
+        }}
+      />
+
+      <Stack.Screen
+        component={Mensajes}
+        name="mensajes"
+        options={{
+          headerTransparent: true,
+          headerTintColor: "#007aff",
+          title: "Mensajes",
+        }}
+      />
+
+      <Stack.Screen
+        component={Contacto}
+        name="contacto"
+        options={{
+          headerTransparent: true,
+          headerTintColor: "#007aff",
+          title: "Contacto",
+        }}
+      />
+    </Stack.Navigator>
+  );
 }
 
 const styles = StyleSheet.create({});
